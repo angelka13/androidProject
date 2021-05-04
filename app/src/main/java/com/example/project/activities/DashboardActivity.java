f331e541afdfd058ac770ca7fbf586c6a5260a74
Angelika <watroba.angelika@gmail.com>
2021-05-04 11:13:30 +0200

    dashboard

--- app/src/main/java/com/example/project/activities/DashboardActivity.java
+++ app/src/main/java/com/example/project/activities/DashboardActivity.java
@@ -1,13 +1,19 @@
 package com.example.project.activities;
 
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
+import com.example.project.R;
+import com.example.project.model.CreditCard;
+
+import java.util.List;
+
 public class DashboardActivity extends AppCompatActivity {
+    private List<CreditCard> creditCards;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
     }
 }
